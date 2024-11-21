f7bd42ce1ee9ead0f8c2c6a4505bbea0199f4d3e
H1ppyyy22 <125800963+H1ppyyy22@users.noreply.github.com>
2024-11-21 19:32:21 +0300

    upload files

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-#TODO: Задание 1
-def sumTwoNums(num1, num2):
-    result = num1 + num2
-    return result
+#TODO Задание 1
+def coordination(x, y):
+    if x > 0 and y > 0:
+        print('I')
+    elif x < 0 and y > 0:
+        print('II')
+    elif x < 0 and y < 0:
+        print('III')
+    elif x > 0 and y < 0:
+        print('IV')
+    
+print(coordination(11, -2))
 
-sum = sumTwoNums(15, 12)
-print(sum)
+#TODO Задание 2
+def ask_password():
+    password = 'password'
+    count = 0
+    while count != 3:
+        user_pass = input('Введите пароль: ')
+        if user_pass != password:
+            print('Неверный пароль!')
+            count += 1
+        elif user_pass == password:
+            print('Пароль принят!')
+            break
+
+ask_password()
